7d36383ed71ae84fcb61d2dfb0cb2571d7e77b10
p <p@w.com>
2016-03-15 12:41:05 -0400

    Initial upload

---
+++ Virtual Crosshair/CrosshairSettingsWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Virtual_Crosshair
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class CrosshairSettingsWindow : Window
+    {
+        protected class CrosshairImage
+        {
+            public string Filename { get; set; }
+            public override string ToString()
+            {
+                return Filename;
+            }
+        }
+        protected List<CrosshairImage> ImageNames = new List<CrosshairImage>();
+        public class CrosshairSettings
+        {
+            public int HorizontalOffset = 0;
+            public int VerticalOffset = 0;
+            public double Scaling = 1.0;
+            public string ImageName;
+        }
+        public delegate void OffsetChangedEventHandler(CrosshairSettings setting);
+        public event OffsetChangedEventHandler OffsetChanged;
+        private CrosshairSettings _currentSettings = new CrosshairSettings();
+        public CrosshairSettingsWindow(string originalImage)
+        {
+            InitializeComponent();
+            LoadImageChoices();
+            _currentSettings.ImageName = originalImage;
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            this.Title = string.Format("Virtual Crosshair v{0} Settings", assembly.GetName().Version);
+            FireChangeEvent();
+        }
+        private void LoadImageChoices()
+        {
+            var imageNames = GetImageNames();
+            foreach (string imageName in imageNames)
+            {
+                this.ImageNames.Add(
+                    new CrosshairImage() { Filename = imageName });
+            }
+            this.ImageChoiceCtl.ItemsSource = this.ImageNames;
+            
+        }
+        private List<string> GetImageNames()
+        {
+            List<string> imageNames = new List<string>();
+            string imagePath = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"\Images\";
+            foreach (string filepath in System.IO.Directory.EnumerateFiles(imagePath + ""))
+            {
+                string filename = System.IO.Path.GetFileName(filepath);
+                imageNames.Add(filename);
+            }
+            return imageNames;
+        }
+
+        private void HorizontalOffsetCtl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (e.NewValue == e.OldValue) { return; }
+            _currentSettings.HorizontalOffset = (int)e.NewValue;
+            txtHorizontal.Text = HorizontalOffsetCtl.Value.ToString();
+            FireChangeEvent();
+            e.Handled = true;
+        }
+        private void VerticalOffsetCtl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (e.NewValue == e.OldValue) { return; }
+            _currentSettings.VerticalOffset = (int)e.NewValue;
+            txtVertical.Text = VerticalOffsetCtl.Value.ToString();
+            FireChangeEvent();
+            e.Handled = true;
+        }
+
+        private void SizeCtl_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (e.NewValue == e.OldValue) { return; }
+            _currentSettings.Scaling = (40.0 + e.NewValue) / 40.0;
+            txtSize.Text = SizeCtl.Value.ToString();
+            FireChangeEvent();
+            e.Handled = true;
+        }
+        private void FireChangeEvent()
+        {
+            if (OffsetChanged != null)
+            {
+                OffsetChanged(_currentSettings);
+            }
+        }
+
+        private void ImageChoiceCtl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this._currentSettings.ImageName = this.ImageChoiceCtl.SelectedValue.ToString();
+            FireChangeEvent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Virtual Crosshair/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using System.Drawing;
+
+namespace Virtual_Crosshair
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CrosshairSettingsWindow _settingsWindow = null;
+        private int _imageHeight;
+        private int _imageWidth;
+        private string originalImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupImage();
+            ScaleTransform(1.0);
+            SetupMonitor();
+            CreateCrosshairSettingsWindow();
+        }
+        private void CreateCrosshairSettingsWindow()
+        {
+            _settingsWindow = new CrosshairSettingsWindow(originalImage);
+            _settingsWindow.OffsetChanged += _settingsWindow_OffsetChanged;
+            _settingsWindow.Show();
+        }
+
+        void _settingsWindow_OffsetChanged(CrosshairSettingsWindow.CrosshairSettings setting)
+        {
+            this.Left = setting.HorizontalOffset;
+            this.Top = (-1 * setting.VerticalOffset);
+            ScaleTransform(setting.Scaling);
+            SetCurrentImage(setting.ImageName);
+        }
+        private void ScaleTransform(double scaling)
+        {
+            imgCrosshair.Height = _imageHeight * scaling;
+            imgCrosshair.Width = _imageWidth * scaling;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+        public void SetupImage()
+        {
+            originalImage = Properties.Settings.Default.Image;
+
+            SetCurrentImage(originalImage);
+        }
+        private void SetCurrentImage(string imageName)
+        {
+
+            string imageDirectory = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) + @"\Images\";
+            string imageFilepath = Path.Combine(imageDirectory, imageName);
+
+            MemoryStream ms = new MemoryStream();
+            using (FileStream fs = File.OpenRead(imageFilepath))
+            {
+                fs.CopyTo(ms);
+            }
+            Bitmap bitmap = new Bitmap(ms);
+            _imageWidth = bitmap.Width;
+            _imageHeight = bitmap.Height;
+
+            Uri imageUri = new Uri(imageFilepath);
+            var bitmapImage = new BitmapImage(imageUri);
+            imgCrosshair.Source = bitmapImage;
+        }
+
+        protected static Stream GetResourceStreamUnused(string resourcePath)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            List<string> resourceNames = new List<string>(assembly.GetManifestResourceNames());
+
+            resourcePath = resourcePath.Replace(@"/", ".");
+            resourcePath = resourceNames.FirstOrDefault(r => r.Contains(resourcePath));
+
+            if (resourcePath == null)
+                throw new FileNotFoundException("Resource not found");
+
+            return assembly.GetManifestResourceStream(resourcePath);
+        }
+        public void SetupMonitor()
+        {
+            int monitor = GetMonitorIndex();
+            System.Windows.Forms.Screen screen = System.Windows.Forms.Screen.AllScreens[monitor];
+
+            Rectangle workingArea = screen.WorkingArea;
+            this.Left = workingArea.Left;
+            this.Top = workingArea.Top;
+            this.Width = workingArea.Width;
+            this.Height = workingArea.Height;
+            this.WindowStyle = WindowStyle.None;
+            this.Show();
+
+        }
+        /// <summary>
+        ///  Return 0-based monitor from user config
+        /// </summary>
+        /// <returns></returns>
+        private int GetMonitorIndex()
+        {
+            if (System.Windows.Forms.Screen.AllScreens.Length == 1) { return 0; }
+            string monitorToUse = Properties.Settings.Default.Monitor;
+            if (EqStr(monitorToUse, "Primary")) { monitorToUse = "1"; }
+            if (EqStr(monitorToUse, "Secondary")) { monitorToUse = "2"; }
+            if (EqStr(monitorToUse, "Tertiary")) { monitorToUse = "3"; }
+            int chosen = 0;
+            if (int.TryParse(monitorToUse, out chosen))
+            {
+                chosen = chosen - 1; // convert 1-based to 0-based
+                if (chosen <= System.Windows.Forms.Screen.AllScreens.Length) { return chosen; }
+            }
+            // If fail to parse or monitor does not exist, just give back 0
+            return 0;
+        }
+        private bool EqStr(string str1, string str2)
+        {
+            return (string.Compare(str1, str2, ignoreCase: true) == 0);
+        }
+
+    }
+}
